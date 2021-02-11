d04633acda10694604025b4bbeabaeca8ea67ead
breno <brenov.dev@gmail.com>
2021-02-11 13:12:33 -0500

    Initial Commit

---
+++ BrenoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrenoCard
+//
+//  Created by user183164 on 11/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
