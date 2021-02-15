0d364adadfbf74dfaf184937eb4dc808826840d9
breno <brenov.dev@gmail.com>
2021-02-15 10:59:38 -0500

    BrenoCard project has bean added

--- BrenoCard/ContentView.swift
+++ BrenoCard/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.20, green: 0.60, blue: 0.86).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Breno")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Breno Vasconcellos")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                    .padding(.bottom)
+                Divider()
+                InfoView(text: "+55 091 98186-2676", imageName: "phone.fill")
+                    .padding(.all)
+                Divider()
+                InfoView(text: "brenov.dev@gmail.com", imageName: "envelope.fill")
+                    .padding(.horizontal)
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 12"))
     }
 }
---
+++ BrenoCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  BrenoCard
+//
+//  Created by user183164 on 11/02/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.blue)
+                Text(text)
+            })
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
